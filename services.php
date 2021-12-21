dc9d2f56bddcb26c80006e2291e12ed8a0049ce3
Quina <2190678@slu.edu.ph>
2021-12-21 11:03:47 +0800

    commit

--- services.php
+++ services.php
@@ -100,7 +100,7 @@
         <script src="https://unpkg.com/scrollreveal"></script>
 
         <!--========== JSCRIPT ==========-->
-        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+        <!-- <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script> -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
         <script src="assets/js/main.js"></script>
     </main>
